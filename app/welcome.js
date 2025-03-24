a68647f66594a0903dab8c6f6cf2645ba5f3514a
Lubasi Milupi <lubasimilupi@gmail.com>
2025-03-24 12:25:41 +0200

    Button and loading state

--- app/welcome.js
+++ app/welcome.js
@@ -1,15 +1,73 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 import React from "react";
 import ScreenWrapper from "../components/ScreenWrapper";
+import { StatusBar } from "expo-status-bar";
+import { hp, wp } from "../helpers/common";
+import { theme } from "../constants/theme";
+import CustomButton from "../components/CustomButton";
 
 const Welcome = () => {
   return (
-    <ScreenWrapper>
-      <Text>Welcome</Text>
+    <ScreenWrapper bg="white">
+      <StatusBar style="dark" />
+      <View style={styles.container}>
+        {/* Welcome Image */}
+        <Image
+          style={styles.welcomeImage}
+          resizeMode="contain"
+          source={require("../assets/images/welcome.png")}
+        />
+        {/* Title and punchline */}
+        <View style={{ gap: 20 }}>
+          <Text style={styles.title}>LinkMe!</Text>
+          <Text style={styles.punchline}>
+            Them way there. Every image finds its own story
+          </Text>
+        </View>
+        {/* Footer */}
+        <View style={styles.footer}>
+          <CustomButton
+            title="Getting started"
+            buttonStyle={{ marginHorizontal: wp(3) }}
+            onPress={() => {}}
+          />
+        </View>
+      </View>
     </ScreenWrapper>
   );
 };
 
 export default Welcome;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "space-around",
+    backgroundColor: "white",
+    paddingHorizontal: wp(4),
+  },
+  title: {
+    color: theme.colors.text,
+    fontSize: hp(4),
+    textAlign: "center",
+    fontWeight: theme.fonts.extrabold,
+  },
+  punchline: {
+    paddingHorizontal: wp(20),
+    fontSize: hp(1.7),
+    textAlign: "center",
+    color: theme.colors.text,
+  },
+
+  welcomeImage: {
+    height: hp(30),
+    width: 100,
+    alignSelf: "center",
+  },
+
+  footer: {
+    gap: 30,
+    width: "100%",
+  },
+});
---
+++ components/CustomButton.js
@@ -0,0 +1,57 @@
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { theme } from "../constants/theme";
+import { hp } from "../helpers/common";
+import loadingState from "./loadingState";
+
+const CustomButton = ({
+  buttonStyle,
+  textStyle,
+  title = "",
+  onPress = () => {},
+  loading = false,
+  hasShadow = true,
+}) => {
+  const shadowStyles = {
+    shadowColor: theme.colors.dark,
+    shadowOffset: { width: 0, height: 10 },
+    shadowOpacity: 0.2,
+    shadowRadius: 8,
+    elevation: 4,
+  };
+
+  if (loading) {
+    return (
+      <View style={[styles.button, buttonStyle, { backgroundColor: "white" }]}>
+        <loadingState />
+      </View>
+    );
+  }
+
+  return (
+    <Pressable
+      onPress={onPress}
+      style={[styles.button, buttonStyle, hasShadow && shadowStyles]}
+    >
+      <Text style={[styles.text, textStyle]}>{title}</Text>
+    </Pressable>
+  );
+};
+
+export default CustomButton;
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: theme.colors.primary,
+    height: hp(6.6),
+    justifyContent: "center",
+    alignItems: "center",
+    borderCurve: "continuous",
+    borderRadius: theme.radius.xl,
+  },
+  text: {
+    fontSize: hp(2.5),
+    color: "white",
+    fontWeight: theme.fonts.bold,
+  },
+});
